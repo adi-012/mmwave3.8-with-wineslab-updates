ef561688c648f3a3738a61c9f0c8bb1fc1e96f45
Aditya Tyagi <zadityagiz@gmail.com>
2024-02-29 11:59:01 +0530

    Update src/mmwave with wineslab code

--- src/mmwave/model/mmwave-flex-tti-mac-scheduler.cc
+++ src/mmwave/model/mmwave-flex-tti-mac-scheduler.cc
@@ -1222,7 +1222,7 @@ MmWaveFlexTtiMacScheduler::DoSchedTriggerReq(
                 {
                     itUeInfo->second.m_ulMcs = mcs; // m_amc->GetMcsFromCqi (cqi);  // get MCS
                 }
-                itUeInfo->second.m_maxUlBufSize = ceBsrIt->second + m_rlcHdrSize + m_macHdrSize + 8;
+                itUeInfo->second.m_maxUlBufSize = ceBsrIt->second + m_rlcHdrSize + m_subHdrSize + 8;
             }
         }
     }
